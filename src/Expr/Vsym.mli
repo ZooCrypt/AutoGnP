f7fc848941d14ffa48996dc7e50821fce939a450
Benedikt Schmidt <beschmi@gmail.com>
2013-10-10 16:05:00 +0200

    add missing Vsym modules

---
+++ src/Expr/Vsym.mli
@@ -0,0 +1,17 @@
+(** Random variable symbols. *)
+open Type
+open IdType
+
+type 'a gt = private { id : 'a Id.gid; ty : 'a gty; }
+type t = internal gt
+type et = exported gt
+val export : t -> et
+val hash : t -> int
+val equal : t -> t -> bool
+val compare : t -> t -> int
+val mk : string -> ty -> t
+val mke : string -> int -> ety -> et
+val pp : Format.formatter -> 'a gt -> unit
+module M : Map.S with type key = t
+module S : Set.S with type elt = t
+module H : Hashtbl.S with type key = t
