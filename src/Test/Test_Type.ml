9f765e53c10d74252ceb5498780e3aa03210b742
Benedikt Schmidt <beschmi@gmail.com>
2013-10-18 20:59:38 +0200

    reenable stats/clear for hashcons (required for test)

--- src/Test/Test_Type.ml
+++ src/Test/Test_Type.ml
@@ -6,10 +6,10 @@ let test_type _ =
   Format.printf "tyvar: %a\n" Lenvar.pp tv;
 
   let ty1_a = mk_ty (BS tv) in
-  let ty1 = mk_ty (Tuple [ty1_a; ty1_a]) in
+  let ty1 = mk_ty (Prod [ty1_a; ty1_a]) in
   Hsty.clear (); (* restart the program and load ty1 from storage. *) 
   let ty2_a = mk_ty (BS tv) in
-  let ty2 = mk_ty (Tuple [ty2_a; ty2_a]) in (* create the same type *)
+  let ty2 = mk_ty (Prod [ty2_a; ty2_a]) in (* create the same type *)
   (* ty1 not equal ty2*)
   Format.printf "ty1=%a,  ty2=%a,  equal=%b \n" pp_ty ty1 pp_ty ty2 (ty_equal ty1 ty2);
   assert_equal ~msg:"ty1 and ty2 not equal" (ty_equal ty1 ty2) false;
