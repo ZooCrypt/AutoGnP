acf2a044f1922d339f406dfc067583dea695560f
Benedikt Schmidt <beschmi@gmail.com>
2013-10-23 19:15:48 +0200

    add solve_xor and the required linear algebra

--- src/CAS/CAS.ml
+++ src/CAS/CAS.ml
@@ -1,8 +1,11 @@
 open Expr
+open Type
 open Poly
+open Util
 open CASLexer
 
 module F = Format
+module L = List
 module Ht = Hashtbl
 
 (* field expression *)
@@ -25,7 +28,7 @@ let rec string_of_fexp e = match e with
 
 (* Abstraction of 'Expr.expr' to sfexp *)
 let rec rename hr = function 
-  | SV i -> SV (Hashtbl.find hr i)
+  | SV i -> SV (Ht.find hr i)
   | (SNat _) as e -> e
   | SOpp e -> SOpp (rename hr e)
   | SInv e -> SInv (rename hr e)
@@ -58,12 +61,12 @@ let abstract_non_field_multiple before es =
   in
   let ses = List.map go es in
   let binding = List.sort e_compare (He.fold (fun e _ l -> e::l) he []) in
-  let hr = Hashtbl.create 17 in
-  let hv = Hashtbl.create 17 in
+  let hr = Ht.create 17 in
+  let hv = Ht.create 17 in
   let c = ref 0 in
   List.iter (fun e -> let i = !c in incr c;
-                      Hashtbl.add hr (He.find he e) i;
-                      Hashtbl.add hv i e) binding;
+                      Ht.add hr (He.find he e) i;
+                      Ht.add hv i e) binding;
   (List.map (rename hr) ses, !c, hv)
 
 let abstract_non_field before e =
@@ -124,7 +127,7 @@ let parse_poly s =
 
 type systems = Singular | Macaulay
 
-let ht_systems = Hashtbl.create 17
+let ht_systems = Ht.create 17
 
 let setup_systems =
   [ (Singular,
@@ -133,12 +136,12 @@ let setup_systems =
 
 let call_system sys cmd linenum =
   let (c_in, c_out) =
-    try Hashtbl.find ht_systems sys
+    try Ht.find ht_systems sys
     with Not_found ->
         let (command,setup) = List.assoc sys setup_systems in
         let cs = Unix.open_process command in
         (match setup with Some s -> output_string (snd cs) s | _ -> ());
-        Hashtbl.add ht_systems sys cs;
+        Ht.add ht_systems sys cs;
         cs
   in
   output_string c_out cmd;
@@ -160,7 +163,6 @@ let import_string (caller : string) (s : string) (hv : (int, expr) Ht.t) =
   exp_of_poly (map_poly (fun i -> try Ht.find hv i
                                   with Not_found -> failwith ("invalid variable returned by "^caller))
               (parse_poly s))
-  
 
 let _norm_singular se c hv =
   let vars = Array.to_list (Array.init c (F.sprintf "x%i")) in
@@ -223,6 +225,74 @@ let mod_reduce a b =
   | repls ->
     failwith (F.sprintf "norm_macaulay: unexpected result %s\n"
                (String.concat "\n" repls))
+
+let direct_subterms o s es = 
+  let go acc e =
+    match e.e_node with
+    | Nary(o',es') when o = o' ->
+      Se.union acc (se_of_list es')
+    | _ ->
+      Se.add e s
+  in
+  List.fold_left go s es
+
+let xor_to_vec rows bindings e =
+  let arr = Array.make rows false in
+  let set_idx e =
+     Array.set arr (Ht.find bindings e) true
+  in
+  (match e.e_node with
+   | Nary(Xor,es') ->
+     List.iter set_idx es'
+   | _ ->
+     set_idx e);
+  arr
+
+(* Try to deduce xor context that deduces e from es *)
+let solve_xor ecs e =
+  let es     = L.map snd ecs in
+  let es_sts = direct_subterms Xor Se.empty es in
+  let rows   = Se.cardinal es_sts in
+  let e_sts  = direct_subterms Xor Se.empty [e] in
+  if (not (Se.subset e_sts es_sts)) then
+    failwith "secret contains subterms that do not occur in known terms";
+    (* raise Not_found; *)
+  let bindings  = Ht.create rows in
+  let ibindings = Ht.create rows in  
+  List.iteri
+    (fun i e ->
+       Ht.add bindings e i;
+       Ht.add ibindings i e)
+    (Se.elements es_sts);
+  let colvecs = List.map (xor_to_vec rows bindings) es in
+  let vec = xor_to_vec rows bindings e in
+  let msolvec = LinAlg.solve colvecs vec in
+  let cs = L.map fst ecs in
+  let ctxt =
+    match msolvec with
+    | None -> failwith "no solution" (*raise Not_found*)
+    | Some(solvec) ->
+        mk_Xor
+          (cat_Some
+            (L.map2 (fun b_i ct -> if b_i then Some ct else None) solvec cs))
+  in
+  Format.printf "%a\n\n%!" pp_exp ctxt;
+  ctxt
+
+let _test () =
+  let l = Lenvar.mk "l" in
+  let t = mk_BS l in
+  let vx = Vsym.mk "x" t in
+  let vy = Vsym.mk "y" t in  
+  let vz = Vsym.mk "z" t in
+  let (x,y,z) = (mk_V vx, mk_V vy, mk_V vz) in
+  let a = mk_Xor [x;y] in
+  let b = mk_Xor [y;z] in  
+  let c = mk_Xor [x;z] in
+  let p1 = mk_V (Vsym.mk "p1" t) in
+  let p2 = mk_V (Vsym.mk "p2" t) in  
+  let c = solve_xor [(p1,a); (p2,b)] c in
+  failwith (fsprintf "Deduced context %a\n%!" pp_exp c |> fsget)
 
 
 let norm before e =
