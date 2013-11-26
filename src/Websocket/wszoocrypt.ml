8793177fc73b697a8c8fd0a85f2e4b9f91a27295
Benedikt Schmidt <beschmi@gmail.com>
2013-11-27 00:42:39 +0100

    update websocket interface

--- src/Websocket/wszoocrypt.ml
+++ src/Websocket/wszoocrypt.ml
@@ -3,17 +3,19 @@ open Util
 open Tactic
 open ParserUtil
 
+module YS = Yojson.Safe
+
 let (>>=) = Lwt.bind
 let (<?>) a b = Lwt.choose [a;b]
 let (<&>) a b = Lwt.join [a;b]
 
 let theory_string = ref ""
 
-(* FIXME: use map or hashtable
-   We use the reversed list of commands (without '.')
-   as the key for the corresponding proofstate.
-*)
+(* We use the reversed list of commands (without '.')
+   as the key for the corresponding proofstate. *)
 let ps_list = ref []
+
+let ps_file = ref ""
 
 let find_ps cmds =
   let rec go handled_cmds rem_cmds =
@@ -26,10 +28,29 @@ let find_ps cmds =
          | last::before -> go before (last::rem_cmds))
   in go cmds []
 
-let processFrame frame =
-  let l = (Util.split (Frame.content frame) '.') |> List.rev |> List.tl in
+let processUnknown s =
+  Lwt_io.printl ("unknown command: ``"^s^"''") >>= fun () ->
+  Lwt.return (Frame.of_string "Unknown command")
+
+let processSave content =
+  Lwt_io.printl ("Save: ``"^content^"''") >>= fun () ->
+  if Sys.file_exists !ps_file then output_file !ps_file content else ();
+  Lwt.return (Frame.of_string (YS.to_string (`Assoc [("cmd", `String "saveOK")])))
+
+let processLoad () =
+  Lwt_io.printl ("Loading") >>= fun () ->
+  (* FIXME: use Lwt *)
+  let s = if Sys.file_exists !ps_file
+          then input_file !ps_file
+          else "(* Enter proof script below *)"
+  in
+  let res = `Assoc [("cmd", `String "setProof"); ("arg", `String s)] in
+  Lwt.return (Frame.of_string (YS.to_string res))
+
+let processEval proofscript = 
+  let l = (Util.splitn proofscript '.') |> List.rev |> List.tl in
   let (ps, rem_cmds) = find_ps l in
-  Lwt_io.printl ("received: ``"^Frame.content frame^"''") >>= fun () -> 
+  Lwt_io.printl ("Eval: ``"^proofscript^"''") >>= fun () -> 
   Lwt_io.printl ("executing "^string_of_int (List.length rem_cmds)^" remaining commands") >>= fun () ->
   let res =
     try (let ps = List.fold_left
@@ -38,29 +59,48 @@ let processFrame frame =
                     ps rem_cmds
          in
          ps_list := (l,ps)::!ps_list;
-         fsprintf "@[<v>current goal:@\n%a@."
-           pp_goals (Util.map_opt (Util.take 1) ps.ps_goals)
-          |> fsget)
-    with Parse.ParseError s -> "parse error: "^s 
+         let g = fsprintf "@[<v>current goal:@\n%a@."
+                   pp_goals (Util.map_opt (Util.take 1) ps.ps_goals)
+                   |> fsget
+         in `Assoc [("cmd", `String "setGoal"); ("arg", `String g)])
+    with Parse.ParseError s ->
+           `Assoc [("cmd", `String "error"); ("arg", `String ("parse error: "^s))]
   in
-  Lwt.return (Frame.of_string res)
+  Lwt.return (Frame.of_string (YS.to_string res))
 
-let server sockaddr =
-  let rec echo_fun uri (stream, push) =
+let processFrame frame =
+  let inp = Frame.content frame in
+  Lwt_io.printl ("Command: ``"^inp^"''") >>= fun () -> 
+  match YS.from_string inp with
+  | `Assoc l ->
+     (try
+        (let get k = List.assoc k l in
+         match get "cmd", get "arg" with
+         | `String "eval", `String pscript -> processEval pscript
+         | `String "load",_                -> processLoad ()
+         | `String "save", `String fcont   -> processSave fcont
+         | _                               -> processUnknown inp)
+      with Not_found -> processUnknown inp)
+  | _ -> processUnknown inp
+
+let run_server node service =
+  let rec zoocrypt_serve uri (stream, push) =
     Lwt_stream.next stream >>= fun frame ->
     processFrame frame >>= fun frame' ->
     Lwt.wrap (fun () -> push (Some frame')) >>= fun () ->
-    echo_fun uri (stream, push) in
-  establish_server sockaddr echo_fun
+    zoocrypt_serve uri (stream, push)
+  in
+  Lwt_io_ext.sockaddr_of_dns node service >>= fun sa ->
+  Lwt.return (establish_server sa zoocrypt_serve)
 
 let rec wait_forever () =
   Lwt_unix.sleep 1000.0 >>= wait_forever
 
 let _ =
-  let run_server node service =
-    Lwt_io_ext.sockaddr_of_dns node service >>= fun sa ->
-    Lwt.return (server sa)
+  let speclist = Arg.align
+    [ ("-f", Arg.Set_string ps_file, " Run server for given file") ]
   in
-  let main () = run_server "localhost" "9999" >>= fun _ -> wait_forever ()
-  in
-  Lwt_main.run (main ())
+  let usage_msg = "Usage: " ^ Sys.argv.(0) ^ " <options>\nOptions are:" in
+  Arg.parse speclist (fun _ -> ()) usage_msg;
+  (if Sys.file_exists !ps_file then Lwt_io.printl ("File: " ^ !ps_file) else Lwt.return ()) >>= fun _ ->  
+  Lwt_main.run (run_server "localhost" "9999" >>= fun _ -> wait_forever ())
