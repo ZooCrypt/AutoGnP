bc10e557397ca3fef440ccdec4e94a22d66bda30
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2015-06-30 17:19:44 +0200

    Fixed 'ju2' Pr_tag and event (equality) in BAD rule.

--- src/Logic/UpToRules.ml
+++ src/Logic/UpToRules.ml
@@ -14,32 +14,33 @@ type oracle =
   | RO of Hsym.t
   | O of Osym.t
            
-let rbad p vsx ju =
+let rbad p vsx ju = (* TODO : Add boolean flag to work with Fail2 rule *)
   (* fail_if_occur vsx ju "rbad"; WHY  IS THIS NEEDED ?*)
   let se = ju.ju_se in
   match get_se_ctxt se p with
   | GLet(vs,e'), se_ctxt when is_H e' ->
-    let h,e = destr_H e' in
-    if not (Hsym.is_ro h) then
-      tacerror "the function %a is not a random oracle" Hsym.pp h;
+     let h,e = destr_H e' in
+     (* check vsx not in fv(e) *)
+     if not (Hsym.is_ro h) then
+       tacerror "the function %a is not a random oracle" Hsym.pp h;
     (*i TODO CHECK THAT h is only used here, and that call are guarded in
        oracle i*)
     
-    let cmds = [GSamp(vs,(e'.e_ty,[]))] in
-    let ju1 = {ju with ju_se = (set_se_ctxt cmds se_ctxt) } in
-    let ev = { ev_quant   = Exists;
-               ev_binding = [[vsx],Oracle.RO(h)];
-               ev_expr    = e } in
-    let ju2 = { ju1 with ju_se = {se with se_ev = ev} } in
-   Rbad(p,vsx), [ju1;ju2]
+     let cmds = [GSamp(vs,(e'.e_ty,[]))] in
+     let ju1 = {ju with ju_se = (set_se_ctxt cmds se_ctxt) } in
+     let ev = { ev_quant   = Exists;
+                ev_binding = [[vsx],Oracle.RO(h)];
+                ev_expr    = mk_Eq (mk_V vsx) e  } in
+     let ju2 = { ju_pr = Pr_Succ; ju_se = {ju1.ju_se with se_ev = ev} } in
+     Rbad(p,vsx), [ju1;ju2]
   | _ ->
-    tacerror "Cannot apply BAD rule : \'Let var(s) = H(expr)\' required."
+     tacerror "Cannot apply BAD rule : \'Let var(s) = H(expr)\' required."
 
 (*
 let rbad_old p vsx ju =
   fail_if_occur vsx ju "rbad";
   let se = ju.ju_se in
-  match get_se_ctxt se p with
+  match get_ju_ctxt se p with
   | GLet(vs,e'), ctxt when is_H e' ->
     let h,e = destr_H e' in
     if not (Hsym.is_ro h) then
