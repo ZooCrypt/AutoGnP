658d41b57ead914b4302357dc4544d0fbc58e56e
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2016-04-25 18:42:28 +0200

    Setting things up for converter functions definitions

--- src/Expr/LeanExpr.ml
+++ src/Expr/LeanExpr.ml
@@ -1,10 +1,19 @@
+open Syms
+open Expr
+       
 module L = Lean
 
+let _TODO _ = failwith "Not implemented"
+             
 module ImportLeanDefs (LF : L.LeanFiles) = struct
   include L.GetExprParser(LF)                                             
   let mk_Eq   = get "eq"        |> as_2ary
   let mk_GExp = get "expr.Exp"  |> as_2ary
   let mk_GGen = get "expr.Ggen"
+  let mk_Z    = _TODO
+  let mk_B =
+    let ltrue,lfalse = (get "true", get "false") in
+    fun b -> if b then ltrue else lfalse
   let mk_FNat : int -> t =
     let nat_zero = get "nat.zero" in
     let nat_succ = get "nat.succ" |> as_1ary in
@@ -31,8 +40,30 @@ module LeanEnv =
       struct
         let _olean = ["data/nat"]
         let _lean = ["../autognp-lean/expr.lean"]
+                      (* TODO : should be read from commandline *)
       end)
-
+module LE = LeanEnv
+              
 type lean_expr = LeanEnv.t
 
-let lean_expr_of_expr = failwith "TODO"
+let mk_Const = function
+  | GGen -> LE.mk_GGen
+  | FNat i -> LE.mk_FNat i
+  | Z -> _TODO ()
+  | B b -> LE.mk_B b
+
+let lean_expr_of_expr_node_leaf = function
+  | V vs -> L.Expr.mk_const @@ VarSym.to_string vs
+  | Cnst c -> mk_Const c
+  | _ -> invalid_arg "e_node leaf (V _ | Cnst _) expected"
+
+module LeanExprOfExpr =
+  Expr.OfExpr
+    (struct
+      type t = lean_expr
+      let of_expr_node_leaf = lean_expr_of_expr_node_leaf
+      let of_op : Expr.op -> t list -> t = _TODO
+    end)
+    
+let lean_expr_of_expr = LeanExprOfExpr.f
+                          
